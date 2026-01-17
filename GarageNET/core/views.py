d98a383c84690ddd4629d71519688527d8810f7b
aniket dede <aniketdede12@gmail.com>
2026-01-17 14:55:20 +0530

    UI: move sidebar toggle button near title

--- GarageNET/core/views.py
+++ GarageNET/core/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, redirect
 from .models import JobCard
 
+
 def dashboard(request):
     return render(request, 'core/index.html', {'active_page': 'dashboard'})
+
 
 def inventory(request):
     return render(request, 'core/inventory.html', {'active_page': 'inventory'})
 
+
 def billing(request):
     return render(request, 'core/bills.html', {'active_page': 'billing'})
+
 
 def jobcards(request):
     jobcards = JobCard.objects.all()
@@ -16,6 +20,7 @@ def jobcards(request):
         'jobcards': jobcards,
         'active_page': 'jobcards'
     })
+
 
 def add_jobcard(request):
     if request.method == 'POST':
